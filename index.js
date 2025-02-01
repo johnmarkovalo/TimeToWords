30b70b85bb082b7d9c4aa3a17f081380b764c02c
John Mark Ovalo <john.ovalo@vocphone.com>
2025-02-01 16:49:02 +0800

    refactor: refactor code

--- index.js
+++ index.js
@@ -1,47 +1,58 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
 function convertTimeToWords(time) {
   // TODO: real code goes here!
-  if (time === "0:00") {
-    return "midnight";
-  }
+  if (time === "0:00") return "midnight";
 
-  if(time === '12:00' ) return 'midday'
+  if (time === "12:00") return "midday";
 
-  const hourMapper = {
-    '1': "one",
-    '2': "two",
-    '3': "three",
-    '4': "four",
-    '5': "five",
-    '6': "six",
-    '7': "seven",
-    '8': "eight",
-    '9': "nine",
-    '10': "ten",
-    '11': "eleven",
-    '12': "twelve",
-  };
+  const [hour, minute] = time.split(":").map(Number);
 
-  const minuteMapper = {
-    "00": "o'clock",
-    "03": "three past",
-    "11": "eleven past",
-    "15": "quarter past",
-    "30": "half past",
-    "33": "twenty seven to",
-    "40": "twenty to",
-    "45": "quarter to",
-    "55": "five to three",
-  };
+  const wordsMapper = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+    "twenty",
+    "twenty-one",
+    "twenty-two",
+    "twenty-three",
+    "twenty-four",
+    "twenty-five",
+    "twenty-six",
+    "twenty-seven",
+    "twenty-eight",
+    "twenty-nine",
+  ];
 
-  const concatedTime = time.split(':');
+  if (minute === 0) return `${wordsMapper[hour]} o'clock`;
+  if (minute === 30) return `half past ${wordsMapper[hour]}`;
+  if (minute === 15) return `quarter past ${wordsMapper[hour]}`;
+  if (minute === 45) return `quarter to ${wordsMapper[(hour % 12) + 1]}`;
 
-  const reverse = concatedTime[1] == '00'
+  const hourWord =
+    minute <= 30 ? wordsMapper[hour] : wordsMapper[(hour % 12) + 1];
+  const minuteWord =
+    minute <= 30 ? wordsMapper[minute] : wordsMapper[60 - minute];
 
-  const convertedHour = Number(concatedTime[1]) <= 30 ? concatedTime[0] : (Number(concatedTime[0]) + 1).toString();
-
-  if( reverse) return hourMapper[convertedHour] + ' ' + minuteMapper[concatedTime[1]];
-  return  minuteMapper[concatedTime[1]] + ' ' + hourMapper[convertedHour];
+  return minute < 30
+    ? `${minuteWord} past ${hourWord}`
+    : `${minuteWord} to ${hourWord}`;
 }
 
 module.exports = { convertTimeToWords };
--- index.test.js
+++ index.test.js
@@ -1,33 +1,38 @@
-const  { convertTimeToWords } = require('./index');
+const { convertTimeToWords } = require("./index");
 
-describe('Time to words', () => {
-  it('Handles midnight', () => {
-    const timeInWords = convertTimeToWords('0:00');
-    expect(timeInWords).toBe('midnight');
+describe("Time to words", () => {
+  it("Handles midnight", () => {
+    const timeInWords = convertTimeToWords("0:00");
+    expect(timeInWords).toBe("midnight");
   });
 
-  it('Handles midday', () => {
-    const timeInWords = convertTimeToWords('12:00');
-    expect(timeInWords).toBe('midday');
+  it("Handles midday", () => {
+    const timeInWords = convertTimeToWords("12:00");
+    expect(timeInWords).toBe("midday");
   });
 
-  it('Handles 30 - 8:30', () => {
-    const timeInWords = convertTimeToWords('8:30');
-    expect(timeInWords).toBe('half past eight');
+  it("Handles 30 - 8:30", () => {
+    const timeInWords = convertTimeToWords("8:30");
+    expect(timeInWords).toBe("half past eight");
   });
 
-  it('Handles times after 30 mins - 2:45', () => {
-    const timeInWords = convertTimeToWords('2:45');
-    expect(timeInWords).toBe('quarter to three');
+  it("Handles times after 30 mins - 2:45", () => {
+    const timeInWords = convertTimeToWords("2:45");
+    expect(timeInWords).toBe("quarter to three");
   });
 
-  it('Handles times before 30 mins - 2:15', () => {
-    const timeInWords = convertTimeToWords('2:15');
-    expect(timeInWords).toBe('quarter past two');
+  it("Handles times after 30 mins - 2:46", () => {
+    const timeInWords = convertTimeToWords("2:46");
+    expect(timeInWords).toBe("fourteen to three");
   });
 
-  it('Handles times exact hour - 2:00', () => {
-    const timeInWords = convertTimeToWords('2:00');
+  it("Handles times before 30 mins - 2:15", () => {
+    const timeInWords = convertTimeToWords("2:15");
+    expect(timeInWords).toBe("quarter past two");
+  });
+
+  it("Handles times exact hour - 2:00", () => {
+    const timeInWords = convertTimeToWords("2:00");
     expect(timeInWords).toBe("two o'clock");
   });
 });
